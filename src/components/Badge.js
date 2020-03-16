22ed5ed047fc9397b4718b65a53eb5878b9925c1
JuanHeis <juaany98@gmail.com>
2020-03-16 19:46:17 -0300

    Clase 11 estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import logoConf from '../img/badge-header.svg'
+import '../styles/styles.css'
 class Badge extends React.Component{
 
     render(){
        return(
-           <div>
-               <div>
+           <div className="Badge">
+               <div className="Badge__header">
                    <img src={logoConf} alt="logo conf   "></img>
                </div>
-               <div>
-                   <img src="" alt="Logo user"></img>
+               <div className="Badge__section-name">
+                   <img className="Badge__avatar" src="" alt="Logo user"></img>
                    <h1>
                        Juan <br></br>H
                    </h1>
                </div>
-               <div>
+               <div className='Badge__section-info'>
                    <p>Soy un programdor</p>
                    <p>@Juan</p>
                </div>
-               <div>
+               <div className="Badge__footer">
                    Platizi conf
                </div>
            </div>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
 
 // const container = document.getElementById('app')
 // container.appendChild(element)
-
+import './global.css'
 import React from 'react'
 import reactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+
 import Badge from './components/Badge'
 const container = document.getElementById('app')
-
 reactDOM.render(<Badge></Badge>, container)
